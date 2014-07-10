f17d67925d3e86f4818324dfe0168b8b4d19b923
Martin Stolz <martin@ustwo.co.uk>
2014-07-10 12:05:07 +0100

    Added initial project scaffolding

---
+++ Scout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scout
+//
+//  Created by Martin on 10/07/2014.
+//  Copyright (c) 2014 ustwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Scout/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Scout
+//
+//  Created by Martin on 10/07/2014.
+//  Copyright (c) 2014 ustwo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
