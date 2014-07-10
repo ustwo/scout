629e181c013fa6ccfffa17de325f4cb5ff761766
Martin Stolz <martin@ustwo.co.uk>
2014-07-10 15:04:56 +0100

    Added Google Analytics tracker

--- ScoutExample/ViewController.m
+++ ScoutExample/ViewController.m
@@ -8,19 +8,62 @@
 
 #import "ViewController.h"
 #import "US2Scout.h"
+#import "GAI.h"
+#import "GAIFields.h"
+#import "GAIDictionaryBuilder.h"
+
+@import CoreData;
+@import SystemConfiguration;
+
+static NSString *const GoogleAnalyticsTrackingId = @"UA-52714436-1";
+static NSString *const GoogleAnalyticsSchemeSeparator = @" ";
 
 
 @implementation ViewController
             
 - (void)viewDidLoad
 {
-    NSSet *schemeSet = [NSMutableSet setWithArray:@[@"kayak", @"trivago", @"ebookers", @"maps", @"safari"]];
+    [self p_setUpTracker];
+    
+    
+    
+    
+    NSSet *schemeSet = [NSMutableSet setWithArray:@[@"kayak", @"trivago", @"ebookers", @"maps", @"http"]];
     
     [US2Scout discoverSchemes:schemeSet withCompletion:^(NSSet *discoveredSchemes) {
         NSLog(@"discoveredSchemes: %@", discoveredSchemes);
+        
+        [self p_trackDiscoveredSchemes:discoveredSchemes];
     }];
     
     [super viewDidLoad];
+}
+
+- (void)p_setUpTracker
+{
+    [GAI sharedInstance].dispatchInterval = 5;
+    [[[GAI sharedInstance] logger] setLogLevel:kGAILogLevelVerbose];
+    [[GAI sharedInstance] trackerWithTrackingId:GoogleAnalyticsTrackingId];
+}
+
+- (void)p_trackDiscoveredSchemes:(NSSet *)schemes
+{
+    id<GAITracker> tracker = [[GAI sharedInstance] defaultTracker];
+    [tracker set:kGAIScreenName value:@"Home Screen"];
+    
+    NSString *trackingLabel = [self p_buildTrackingLabelFromSet:schemes];
+    NSDictionary *eventData = [[GAIDictionaryBuilder createEventWithCategory:@"scout"
+                                                                      action:@"discovered"
+                                                                       label:trackingLabel
+                                                                       value:nil] build];
+    [tracker send:eventData];
+}
+
+- (NSString *)p_buildTrackingLabelFromSet:(NSSet *)set
+{
+    NSString *trackingLabel = [[set allObjects] componentsJoinedByString:GoogleAnalyticsSchemeSeparator];
+    
+    return trackingLabel;
 }
 
 @end
